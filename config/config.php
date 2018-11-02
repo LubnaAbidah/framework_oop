47989baeddbac97a245a31b3a529a97fc3d945ca
Lubna Abidah <lubna16abidah@gmail.com>
2018-11-02 15:04:50 +0700

    Add files via upload
    Versi 2.0

--- config/config.php
+++ config/config.php
@@ -1,10 +1,9 @@
 <?php
-define('DEVELOPMENT_ENVIRONMENT', true); //mengatur apakah aplikasi berada dalam tahap pengembangan, nilai true jika iya
-define('BASE_PATH', 'http://localhost/crud'); //menentukan Base URL
-define('DEFAULT_CONTROLLER', 'produk'); //menentukan default controller
-//konfigurasi database
-define('DB_NAME','makanan'); //nama database
-define('DB_USER', 'root'); //username database
-define('DB_PASSWORD', ''); //password database
-define('DB_HOST', 'localhost'); //mengatur host database
-?>
+define('DEVELOPMENT_ENVIRONMENT', true);
+define('BASE_PATH', 'http://localhost/crud');
+define('DEFAULT_CONTROLLER', 'produk');
+
+define('DB_NAME', 'toko_online');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
--- library/model.class.php
+++ library/model.class.php
@@ -1,49 +1,55 @@
-<?php 
-class Model {
+<?php
+class Model{
 	protected $_dbHandle;
 	protected $_table;
-
-	//koneksi ke database
+	
+//Koneksi ke database	
 	public function connect(){
 		$this->_dbHandle = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-		if(mysqli_connect_error()){
-			echo "Failed to connect database :".mysqli_connect_error();
+		
+		if(mysqli_connect_errno()){
+			echo "Failed to connect database: ".mysqli_connect_error();
 		}
 	}
 
-	//method untuk proses query
-	public function query($query) {
+//Method untuk memproses hasil query	
+	public function query($query){
 		return mysqli_query($this->_dbHandle, $query);
 	}
+	
 	public function getResult($mysqliQuery){
 		$data = array();
-		while ($record = mysqli_fetch_array($mysqliQuery)) {
+		while($record = mysqli_fetch_array($mysqliQuery)){
 			array_push($data, $record);
 		}
 		return $data;
 	}
+	
 	public function getRows($mysqliQuery){
 		return mysqli_num_rows($mysqliQuery);
 	}
+	
 	public function getError(){
 		return mysqli_error($this->_dbHandle);
 	}
+	
 	public function getId(){
 		return mysqli_insert_id($this->_dbHandle);
 	}
-
-	//method untuk menampilkan data
+	
+//Method untuk menampilkan data		
 	public function selectAll($orderBy='', $order='ASC', $limit=''){
-		$query ="SELECT * FROM ".$this->_table;
-		if($orderBy!='') $query .=" ORDER BY $orderBy $order";
-		if($limit!='') $query .= "LIMIT $limit";
-
+		$query = "SELECT * FROM ".$this->_table;
+		
+		if($orderBy!='') $query .= " ORDER BY $orderBy $order";
+		if($limit!='') $query .= " LIMIT $limit";
+		
 		return $this->query($query);
 	}
-	public function selectWhere($condition=array(), $orderBy='', $order='ASC', $limit=''){
+	
+	public function selectWhere($condition='', $orderBy='', $order='ASC', $limit=''){
 		$query = "SELECT * FROM ".$this->_table;
-
+		
 		if(is_array($condition)){
 			$query .= " WHERE";
 			foreach($condition as $key=>$val){
@@ -51,14 +57,16 @@ class Model {
 			}
 			$query = substr($query, 0, -3);
 		}
-		if($orderBy!='') $query .=" ORDER BY $orderBy $order";
-		if($limit!='') $query .= "LIMIT $limit";
-
+		
+		if($orderBy!='') $query .= " ORDER BY $orderBy $order";
+		if($limit!='') $query .= " LIMIT $limit";
+		
 		return $this->query($query);
 	}
+	
 	public function selectLike($condition='', $orderBy='', $order='ASC', $limit=''){
 		$query = "SELECT * FROM ".$this->_table;
-
+		
 		if(is_array($condition)){
 			$query .= " WHERE";
 			foreach($condition as $key=>$val){
@@ -66,74 +74,84 @@ class Model {
 			}
 			$query = substr($query, 0, -2);
 		}
-		if($orderBy!='') $query .=" ORDER BY $orderBy $order";
-		if($limit!='') $query .= "LIMIT $limit";
-
+		
+		if($orderBy!='') $query .= " ORDER BY $orderBy $order";
+		if($limit!='') $query .= " LIMIT $limit";
+		
 		return $this->query($query);
 	}
-	public function selectjoin($table, $join="JOIN", $param, $condition='', $orderBy='', $order='ASC', $limit=''){
+	
+	public function selectJoin($table, $join="JOIN", $param, $condition='', $orderBy='', $order='ASC', $limit=''){
 		$query = "SELECT * FROM ".$this->_table;
-
+		
 		if(is_array($table)){
-			foreach ($table as $tbl) {
+			foreach($table as $tbl){
 				$query .= " $join $tbl";
 			}
-		}else{ $query .= "$join $table";
-			foreach ($param as $key => $val) {
-				$query .= " ON $key= $val";
-			}
+		}else $query .= " $join $table";
+		
+		foreach($param as $key=>$val){
+			$query .= " ON $key=$val";
 		}
+			
 		if(is_array($condition)){
 			$query .= " WHERE";
 			foreach($condition as $key=>$val){
 				$query .= " $key='$val' AND";
-			}		
-			$query = substr($query, 0, -3);
-		}
-		if($orderBy!='') $query .=" ORDER BY $orderBy $order";
-		if($limit!='') $query .= "LIMIT $limit";
-
-		return $this->query($query);
-	}
-	//METHOD MENGHAPUS DATA
-	public function delete($condition){
-		$query = "DELETE FROM ".$this->_table;
-
-		if(is_array($condition)){
-			$query .= " WHERE";
-			foreach ($condition as $key => $val) {
-				$query .= " $key='$val' AND";
 			}
 			$query = substr($query, 0, -3);
 		}
-		return $this->query($query);
-	}
-	//METHOD MENAMBAH DATA
-	public function insert($data){
-		$query = "INSERT INTO ".$this->_table." SET";
-
-			foreach ($data as $key => $val) {
-				$query .= " $key='$val',";
-			}
-			$query = substr($query, 0, -1);
+		
+		if($orderBy!='') $query .= " ORDER BY $orderBy $order";
+		if($limit!='') $query .= " LIMIT $limit";
 		
 		return $this->query($query);
 	}
-	//METHOD MENGUBAH DATA
-	public function update($data, $condition){
-		$query = "UPDATE ".$this->_table." SET";
-			foreach ($data as $key => $val) {
-				$query .= " $key='$val',";
-			}
-			$query = substr($query, 0, -1);
+
+//Method untuk menghapus data	
+	public function delete($condition=''){
+		$query = "DELETE FROM ".$this->_table;
+		
 		if(is_array($condition)){
 			$query .= " WHERE";
-			foreach ($condition as $key => $val) {
+			foreach($condition as $key=>$val){
 				$query .= " $key='$val' AND";
 			}
 			$query = substr($query, 0, -3);
 		}
+		
 		return $this->query($query);
 	}
-
+	
+//Method untuk menambah data		
+	public function insert($data){
+		$query = "INSERT INTO ".$this->_table." SET";
+		
+		foreach($data as $key=>$val){
+			$query .= " $key='$val',";
+		}
+		$query = substr($query, 0, -1);
+		
+		return $this->query($query);
+	}
+	
+//Method untuk memperbaiki data		
+	public function update($data, $condition=''){
+		$query = "UPDATE ".$this->_table." SET";
+		
+		foreach($data as $key=>$val){
+			$query .= " $key='$val',";
+		}
+		$query = substr($query, 0, -1);
+		
+		if(is_array($condition)){
+			$query .= " WHERE";
+			foreach($condition as $key=>$val){
+				$query .= " $key='$val' AND";
+			}
+			$query = substr($query, 0, -3);
+		}
+		
+		return $this->query($query);
+	}
 }
